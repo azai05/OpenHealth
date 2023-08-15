3785d8fefcdd79ae074afc0e8e026c76c1a25d94
Amelia Zai <zaiamelia@gmail.com>
2023-08-15 11:12:30 -0400

    Initial Commit

---
+++ OpenHealth/OpenHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenHealthApp.swift
+//  OpenHealth
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct OpenHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
